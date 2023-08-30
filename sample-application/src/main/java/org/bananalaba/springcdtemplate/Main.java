5b78615cf7228ceaa2917617646b774306dea09d
Judzin <foo.lord@protonmail.com>
2023-08-30 08:28:43 +0300

    Apply centralised Maven-style dependency management.

--- sample-application/src/main/java/org/bananalaba/springcdtemplate/Main.java
+++ sample-application/src/main/java/org/bananalaba/springcdtemplate/Main.java
@@ -1,8 +1,11 @@
 package org.bananalaba.springcdtemplate;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 public class Main {
 
     public static void main(String[] arguments) {
+        checkArgument(arguments.length == 0, "no arguments expected");
         System.out.println("Hello, world");
     }
 
