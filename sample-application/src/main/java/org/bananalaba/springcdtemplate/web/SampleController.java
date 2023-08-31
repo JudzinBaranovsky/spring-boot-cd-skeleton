3a9c8623ad4a188455caede94da6200a043c0a42
Judzin <foo.lord@protonmail.com>
2023-08-31 08:38:43 +0300

    Switch logging to Logback and configure a rolling log file appender.

--- sample-application/src/main/java/org/bananalaba/springcdtemplate/web/SampleController.java
+++ sample-application/src/main/java/org/bananalaba/springcdtemplate/web/SampleController.java
@@ -1,5 +1,6 @@
 package org.bananalaba.springcdtemplate.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.bananalaba.springcdtemplate.model.SampleDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/sample")
+@Slf4j
 public class SampleController {
 
     @GetMapping(path = "/hello", produces = "application/json")
     public SampleDto hello() {
+        log.info("received a hello request");
         return new SampleDto("Hello, World");
     }
 
