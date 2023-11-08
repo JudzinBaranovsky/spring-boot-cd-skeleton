897e7491a064f2fe1ec440f2c01d17b117ebcf22
Judzin <foo.lord@protonmail.com>
2023-11-08 19:07:35 +0300

    Fix a security bug in the client declaration. Downgrade Gradle wrapper to fix the compatibility with SpotBugs.

--- sample-application/src/main/java/org/bananalaba/springcdtemplate/client/TipsClient.java
+++ sample-application/src/main/java/org/bananalaba/springcdtemplate/client/TipsClient.java
@@ -2,11 +2,12 @@ package org.bananalaba.springcdtemplate.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = "tipsServiceClient", url = "${service.integration.tipsService.url}")
 public interface TipsClient {
 
-    @RequestMapping("/tip-of-the-day")
+    @RequestMapping(method = RequestMethod.GET, path = "/tip-of-the-day")
     String getTipOfTheDay();
 
 }
