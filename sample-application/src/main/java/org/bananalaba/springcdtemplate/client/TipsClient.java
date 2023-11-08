64104af497c17c9009772e1c41c5ed8f71d3382f
Judzin <foo.lord@protonmail.com>
2023-11-08 18:48:21 +0300

    Disable unnecessary client-side load-balancing.

--- sample-application/src/main/java/org/bananalaba/springcdtemplate/client/TipsClient.java
+++ sample-application/src/main/java/org/bananalaba/springcdtemplate/client/TipsClient.java
@@ -3,7 +3,7 @@ package org.bananalaba.springcdtemplate.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("tipsService")
+@FeignClient(name = "tipsServiceClient", url = "${service.integration.tipsService.url}")
 public interface TipsClient {
 
     @RequestMapping("/tip-of-the-day")
