114b0f190c54b56722583763213315635b72299f
Judzin <foo.lord@protonmail.com>
2023-11-27 08:32:08 +0300

    Replace manual logging with a declarative aspect.

--- sample-application/src/main/java/org/bananalaba/springcdtemplate/web/SampleController.java
+++ sample-application/src/main/java/org/bananalaba/springcdtemplate/web/SampleController.java
@@ -2,8 +2,8 @@ package org.bananalaba.springcdtemplate.web;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.bananalaba.springcdtemplate.client.NumberServiceClient;
+import org.bananalaba.springcdtemplate.logging.Loggable;
 import org.bananalaba.springcdtemplate.model.SampleDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/sample")
-@Slf4j
 @RequiredArgsConstructor
+@Loggable
 public class SampleController {
 
     @NonNull
@@ -24,8 +24,6 @@ public class SampleController {
 
     @GetMapping(path = "/hello", produces = "application/json")
     public SampleDto hello() {
-        log.info("received a hello request");
-
         var luckyNumber = numberServiceClient.getRandomNumber();
         return new SampleDto("Hello, World", nodeIp, luckyNumber);
     }
