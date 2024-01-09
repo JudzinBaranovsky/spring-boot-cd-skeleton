ddda15e07f9df137a9699dce8888c0c531a3702c
Judzin <foo.lord@protonmail.com>
2024-01-09 12:46:48 +0300

    Add a web UI module to serve as an OAuth2 client.

---
+++ message-store-ui/src/main/java/org/bananalaba/messagestore/MessageStoreUiApplication.java
@@ -0,0 +1,13 @@
+package org.bananalaba.messagestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageStoreUiApplication {
+
+    public static void main(String[] arguments) {
+        SpringApplication.run(MessageStoreUiApplication.class, arguments);
+    }
+
+}
---
+++ message-store-ui/src/main/java/org/bananalaba/messagestore/web/HomeController.java
@@ -0,0 +1,17 @@
+package org.bananalaba.messagestore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/web")
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        return "index";
+    }
+
+}
