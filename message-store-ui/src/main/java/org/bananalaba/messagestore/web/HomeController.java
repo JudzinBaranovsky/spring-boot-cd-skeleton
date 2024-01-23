737a88dc0a2f61dd94833984bfcf18ee6790710f
Judzin <foo.lord@protonmail.com>
2024-01-23 08:12:54 +0300

    Add UI controls for interacting with the message store. Allow CORS requests between the UI application and message API. Fix tracing sampling for the UI app.

--- message-store-ui/src/main/java/org/bananalaba/messagestore/web/HomeController.java
+++ message-store-ui/src/main/java/org/bananalaba/messagestore/web/HomeController.java
@@ -1,5 +1,8 @@
 package org.bananalaba.messagestore.web;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/web")
 public class HomeController {
 
+    @Value("${message.api.host}")
+    private String messageApiHost;
+
     @GetMapping("/home")
-    public String home(Model model) {
+    public String home(Model model, @RegisteredOAuth2AuthorizedClient("okta") OAuth2AuthorizedClient client) {
+        model.addAttribute("authToken", "<not implemented yet>");
+        model.addAttribute("messageApiHost", messageApiHost);
+
         return "index";
     }
 
--- sample-application/src/main/java/org/bananalaba/springcdtemplate/config/SecurityConfig.java
+++ sample-application/src/main/java/org/bananalaba/springcdtemplate/config/SecurityConfig.java
@@ -2,17 +2,25 @@ package org.bananalaba.springcdtemplate.config;
 
 import static org.springframework.http.HttpMethod.DELETE;
 import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.OPTIONS;
 import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.security.config.Customizer.withDefaults;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${web.cors.allowedOrigins}")
+    private String corsAllowedOrigins;
 
     @Bean
     public SecurityFilterChain messageApiSecurityChain(HttpSecurity security) throws Exception {
@@ -20,9 +28,22 @@ public class SecurityConfig {
                 .requestMatchers(GET, "/api/v1/messages/**").hasAuthority("SCOPE_read:message")
                 .requestMatchers(DELETE, "api/v1/messages/**").hasAuthority("SCOPE_delete:message")
                 .requestMatchers(POST, "api/v1/messages/**").hasAuthority("SCOPE_write:message")
+                .requestMatchers(OPTIONS, "api/v1/messages/**").permitAll()
             )
-            .oauth2ResourceServer(server -> server.jwt(jwt -> {}))
+            .oauth2ResourceServer(server -> server.jwt(withDefaults()))
             .build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/**").allowedOrigins(corsAllowedOrigins);
+            }
+
+        };
     }
 
 }
