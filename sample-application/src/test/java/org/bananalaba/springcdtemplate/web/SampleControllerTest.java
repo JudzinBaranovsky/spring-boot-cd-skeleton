10ca290026beb4644c0b8fda0bbff5b9c124baca
Judzin <foo.lord@protonmail.com>
2025-02-07 19:49:48 +0300

    Fix a unit test.

--- sample-application/src/test/java/org/bananalaba/springcdtemplate/web/SampleControllerTest.java
+++ sample-application/src/test/java/org/bananalaba/springcdtemplate/web/SampleControllerTest.java
@@ -1,18 +1,31 @@
 package org.bananalaba.springcdtemplate.web;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 import org.bananalaba.springcdtemplate.model.QuoteDto;
+import org.bananalaba.springcdtemplate.service.QuotesService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class SampleControllerTest {
 
-    private final SampleController controller = new SampleController("192.168.0.1");
+    @Mock
+    private QuotesService quotesService;
+    @InjectMocks
+    private SampleController controller;
 
     @Test
     public void shouldGreet() {
+        var expected = new QuoteDto("Hello, World");
+        when(quotesService.getRandomQuote()).thenReturn(expected);
+
         var actual = controller.hello();
-        assertThat(actual).isEqualTo(new QuoteDto("Hello, World", "192.168.0.1"));
+        assertThat(actual).isEqualTo(expected);
     }
 
 }
