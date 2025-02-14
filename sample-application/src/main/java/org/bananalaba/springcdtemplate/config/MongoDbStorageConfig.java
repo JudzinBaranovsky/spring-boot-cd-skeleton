13927956c6598f20d7bdf9d40146c3c06e5b5323
Yauhen Baranouski <yauhen_baranouski@epam.com>
2025-02-14 10:00:35 +0300

    Set up MongoDB schema migrations management with Liquibase.

--- sample-application/src/main/java/org/bananalaba/springcdtemplate/config/MongoDbStorageConfig.java
+++ sample-application/src/main/java/org/bananalaba/springcdtemplate/config/MongoDbStorageConfig.java
@@ -1,17 +1,25 @@
 package org.bananalaba.springcdtemplate.config;
 
+import liquibase.Liquibase;
+import liquibase.database.DatabaseFactory;
+import liquibase.ext.mongodb.database.MongoLiquibaseDatabase;
+import liquibase.resource.ClassLoaderResourceAccessor;
 import lombok.extern.slf4j.Slf4j;
 import org.bananalaba.springcdtemplate.service.DocumentOrderService;
 import org.bananalaba.springcdtemplate.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoConnectionDetails;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoOperations;
 
 @Configuration
 @Profile("mongodb")
 @Slf4j
+@Import(LiquibaseAutoConfiguration.class)
 public class MongoDbStorageConfig {
 
     @Bean
@@ -20,9 +28,18 @@ public class MongoDbStorageConfig {
     }
 
     @Autowired
-    public void makeTestDbCall(final MongoOperations dbOps) {
-        dbOps.createCollection("test-items");
-        var result = dbOps.collectionExists("test-items");
+    public void applySchemaMigrations(final MongoConnectionDetails connectionDetails, final MongoOperations dbOps) throws Exception {
+        MongoLiquibaseDatabase database = (MongoLiquibaseDatabase) DatabaseFactory.getInstance().openDatabase(
+            connectionDetails.getConnectionString().getConnectionString(),
+            null,
+            null,
+            null,
+            null
+        );
+        Liquibase liquibase = new Liquibase("migrations/mongodb/liquibase/changelog-master.yaml", new ClassLoaderResourceAccessor(), database);
+        liquibase.update("");
+
+        var result = dbOps.collectionExists("orders");
         log.info("test MongoDB collection created: {}", result);
     }
 
