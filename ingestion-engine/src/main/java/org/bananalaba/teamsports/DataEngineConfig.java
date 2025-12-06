0efcf923d7dd51ad8008e96030d6a94f06607665
Yauhen Baranouski <yauhen_baranouski@epam.com>
2025-12-06 19:39:56 +0300

    Ensure timeouts for external integrations.

--- ingestion-engine/src/main/java/org/bananalaba/teamsports/DataEngineConfig.java
+++ ingestion-engine/src/main/java/org/bananalaba/teamsports/DataEngineConfig.java
@@ -1,8 +1,12 @@
 package org.bananalaba.teamsports;
 
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -10,8 +14,13 @@ import org.springframework.web.client.RestTemplate;
 public class DataEngineConfig {
 
     @Bean("dataSourceRestTemplate")
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(@Value("${soccer.api.connectionTimeoutMs:30000}") final int connectionTimeoutMs,
+                                     @Value("${soccer.api.socketTimeoutMs:30000}")final int socketTimeoutMs) {
+        var requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(Duration.ofMillis(connectionTimeoutMs));
+        requestFactory.setReadTimeout(Duration.ofMillis(socketTimeoutMs));
+
+        return new RestTemplate(requestFactory);
     }
 
 }
